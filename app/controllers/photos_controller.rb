29292c4a575fe80046d916becb5fba0d713b9c68
Dhruti <dhruti.p@simformsolutions.com>
2021-10-13 11:31:34 +0530

    add identation

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,42 +1,42 @@
 class PhotosController < ApplicationController
-	def index
-		@photos = Photo.order('created_at')
-	end
+  def index
+    @photos = Photo.order('created_at')
+  end
 
-	def new
-		@photo = Photo.new
-	end
+  def new
+    @photo = Photo.new
+  end
 
-	def create
-		@photo = Photo.new(photo_params)
-		if @photo.save
-			flash[:notice] = "Successfully added new photo!"
-			redirect_to photos_path
-		else
-			flash[:alert] = "Error adding new photo!"
-			render :new
-		end
-	end
+  def create
+    @photo = Photo.new(photo_params)
+    if @photo.save
+      flash[:notice] = "Successfully added new photo!"
+      redirect_to photos_path
+    else
+      flash[:alert] = "Error adding new photo!"
+      render :new
+    end
+  end
 
-	def destroy
-		@photo = Photo.find(params[:id])
-		if @photo.destroy
-			flash[:notice] = "Successfully deleted photo!"
-			redirect_to photos_path
-		else
-			flash[:alert] = "Error deleting photo!"
-		end
-	end
+  def destroy
+    @photo = Photo.find(params[:id])
+    if @photo.destroy
+      flash[:notice] = "Successfully deleted photo!"
+      redirect_to photos_path
+    else
+      flash[:alert] = "Error deleting photo!"
+    end
+  end
 
-	def show
-		@photo = Photo.find(params[:id])
-		@prev_photo = @photo.prev
-		@next_photo = @photo.next
-	end
+  def show
+    @photo = Photo.find(params[:id])
+    @prev_photo = @photo.prev
+    @next_photo = @photo.next
+  end
 
-	private
+  private
 
-	def photo_params
-		params.require(:photo).permit(:title, :image, :user_id)
-	end
+  def photo_params
+    params.require(:photo).permit(:title, :image, :user_id)
+  end
 end
