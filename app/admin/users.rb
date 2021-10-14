2a02d757f555b3a3200fc3787d23c331e4b4486e
Dhruti <dhruti.p@simformsolutions.com>
2021-10-14 11:59:20 +0530

    add styling

--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,18 +1,30 @@
 ActiveAdmin.register User do
+  config.sort_order = 'id_asc'
+  permit_params :id, :email, :created_at, :updated_at, :confirmed_at, :confirmation_token, :confirmation_sent_at
+  filter :email
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:email, :encrypted_password, :reset_password_token, :reset_password_sent_at, :remember_created_at, :confirmation_token, :confirmed_at, :confirmation_sent_at, :unconfirmed_email]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    selectable_column
+    id_column
+    column :email
+    column :created_at
+    column :updated_at
+    column :confirmation_token
+    column :confirmed_at
+    column :confirmation_sent_at
+    actions
+  end
+  form do |f|
+    f.inputs do
+      f.input :id
+      f.input :email
+      f.input :created_at
+      f.input :updated_at
+      f.input :confirmation_token
+      f.input :confirmed_at
+      f.input :confirmation_sent_at
+    end
+    f.actions
+  end
+
 end
